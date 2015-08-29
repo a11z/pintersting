dd9b98108f757ed99316e396fe45fba1c983f02f
Sam <gibsonorbust@gmail.com>
2015-08-29 17:02:00 +0300

    add s3 for paperclip

--- Gemfile
+++ Gemfile
@@ -26,6 +26,8 @@ gem 'jbuilder', '~> 2.0'
 gem 'devise', '~> 3.5.2'
 
 gem 'paperclip', '~> 4.2'
+
+gem 'aws-sdk', '< 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,13 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'narrrate.com' }
   # Required for heroku
   # SET TO ACTUAL HOST NAME!
+    config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+    # sets paperclip to upload to amazon s3
+  }
 end
